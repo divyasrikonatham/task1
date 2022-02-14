6b56d9d572279495ccaa444610f3b0c1920e2935
Chandrasekhar <divyasri4435@gmail.com>
2022-02-14 16:08:31 +0530

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        public static bool checkPalin(string word)
+        {
+            int n = word.Length;
+            word = word.ToLower();
+            for (int i = 0; i < n; i++, n--)
+            {
+                if (word[i] != word[n - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static int countPalin(string str)
+        {
+
+            str = str + " ";
+
+            // to store each word
+            string word = "";
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+
+                // extracting each word
+                if (ch != ' ')
+                {
+                    word = word + ch;
+                }
+                else
+                {
+                    if (checkPalin(word))
+                    {
+                        count++;
+                    }
+                    word = "";
+                }
+            }
+
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter 500 words description ");
+            string str = Console.ReadLine();
+            Regex regex = new Regex("[!@#$%^&*]");
+            bool hasSpecialChars = regex.IsMatch(str);
+            if (hasSpecialChars == true)
+            {
+                Console.WriteLine("Please re-enter 500 words description without any of the !@#$%^&* special characters");
+            }
+            else
+            {
+                int i = 0, wordCount = 1;
+                while (i <= str.Length - 1)
+                {
+                    if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t')
+                    {
+                        wordCount++;
+                    }
+                    i++;
+                }
+                Console.WriteLine("Number of words in the description is {0}", wordCount);
+                Console.WriteLine("Number of palindromes in the description is {0}", countPalin(str));
+                var outputPath = @"D:\filesIntro\user_input.txt";
+                File.AppendAllText(outputPath, str);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+        
+    
+
